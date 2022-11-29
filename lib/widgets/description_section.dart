9cd17d810cc04b3e8fbd0ae6eecbd876af4e59f3
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-11-30 01:18:30 +0600

    list added

---
+++ lib/widgets/description_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DescriptionSection extends StatelessWidget {
+  const DescriptionSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/widgets/videos_section.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class VideoSection extends StatelessWidget {
+  List videoList = [
+    'Indroduction to Flutter',
+    'Installing Flutter on Windows',
+    'Setup Emulator on Window',
+    'Creating Our First App'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: videoList.length,
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemBuilder: (context, index){
+        return ListTile(
+          leading: Container(
+            padding: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: index == 0
+                  ? Color(0xff674aef)
+                  :  Color(0xff674aef).withOpacity(0.6),
+              shape: BoxShape.circle,
+            ),
+            child: Icon(
+              Icons.play_arrow_rounded,
+              color: Colors.white,
+                size: 30,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
